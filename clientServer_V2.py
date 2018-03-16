a4cda6fe9e4bc60614073f88526a2f4c7e6c5939
Nathan Vico <nathan.vico97@gmail.com>
2018-03-16 15:45:00 +0100

    add info text and fix while condition

---
+++ clientServer_V2.py
@@ -0,0 +1,23 @@
+import socket
+import os
+
+host = "localhost" #changer localhost par l'ipv4 de la machine sur laquelle se trouve le serveur
+port = 6000
+
+t=socket.socket()
+t.connect((host,port))
+
+print("connecté au server sur le port {}".format(port))
+
+sendata = b""
+
+while True:
+    sendata = input("> ")
+    sendata = sendata.encode()
+    t.send(sendata)
+    recvdata = t.recv(1024)
+    print(recvdata.decode())
+    if recvdata.decode() == "chat openned" :
+        os.system("chat_V1.py")
+
+t.close()
